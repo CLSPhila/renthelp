d747ebc1fe3bf1a3333d5adf61f4e8369ba0a912
Nate Vogel <nathan.c.vogel@gmail.com>
2020-05-13 16:15:36 -0400

    updated config for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `cls-rental-assistance-screener`,
+        short_name: `rent-help`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#454988`,
+        theme_color: `#454988`,
         display: `minimal-ui`,
         icon: `src/images/CLS-Logo_120.png`, // This path is relative to the root of the site.
       },
@@ -48,6 +48,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
